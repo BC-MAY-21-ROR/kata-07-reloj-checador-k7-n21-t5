acf48abd97f0071aa78701191d841e68f6861c54
javs1 <59986245+javs1@users.noreply.github.com>
2022-01-06 17:17:39 -0600

    Adding initial desing
    Added a calendar to represent the employee attendance by day

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,6 +9,7 @@
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 //= require employee
 =======
 =======
@@ -17,6 +18,8 @@
 //= require employee
 =======
 >>>>>>> 8696622 (Adding initial desing)
+=======
+>>>>>>> 3af6f1d (Adding initial desing)
 //= require moment 
 //= require fullcalendar
 //= require fullcalendar/locale-all
@@ -24,10 +27,13 @@
 $('#calendar').fullCalendar({});
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 >>>>>>> 3af6f1d (Adding initial desing)
 =======
 =======
 >>>>>>> 8696622 (Adding initial desing)
+>>>>>>> 3af6f1d (Adding initial desing)
+=======
 >>>>>>> 3af6f1d (Adding initial desing)
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
 <<<<<<< HEAD
+<<<<<<< HEAD
   get 'crud', to: "employee_crud#index"
   get 'crud/:id', to: "employee_crud#index"
 =======
 >>>>>>> 8696622 (Adding initial desing)
+=======
+>>>>>>> 3af6f1d (Adding initial desing)
   get 'reports/index', to: "reports#index"
   get 'admin', to: "home#index"
   post 'admin', to: "home#login"
