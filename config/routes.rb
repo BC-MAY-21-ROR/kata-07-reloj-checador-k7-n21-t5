51fc0ca530dc0cd92eb1aa8f35242f60779ccf0a
javs1 <59986245+javs1@users.noreply.github.com>
2022-01-06 17:17:39 -0600

    Adding initial desing
    Added a calendar to represent the employee attendance by day

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,7 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
-  get 'employee_status/new'
-  get 'employee_status/create'
   get 'crud', to: "employee_crud#index"
   get 'crud/:id', to: "employee_crud#index"
-=======
-<<<<<<< HEAD
-  get 'crud', to: "crud#index"
-=======
   get 'reports/index', to: "reports#index"
->>>>>>> 8696622 (Adding initial desing)
->>>>>>> 2fe59be (Adding initial desing)
   get 'admin', to: "home#index"
   post 'admin', to: "home#login"
   post 'logout', to: "employee_crud#logout"
