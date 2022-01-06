75f0f935f4090b9298572cbc338ad871cd4e7452
javs1 <59986245+javs1@users.noreply.github.com>
2022-01-06 17:17:39 -0600

    Adding initial desing
    Added a calendar to represent the employee attendance by day

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,13 +7,19 @@
 //= require_tree
 //= require bootstrap-sprockets
 <<<<<<< HEAD
+<<<<<<< HEAD
 //= require employee
 =======
+=======
+>>>>>>> 3af6f1d (Adding initial desing)
 //= require moment 
 //= require fullcalendar
 //= require fullcalendar/locale-all
 
 $('#calendar').fullCalendar({});
+<<<<<<< HEAD
+>>>>>>> 3af6f1d (Adding initial desing)
+=======
 >>>>>>> 3af6f1d (Adding initial desing)
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
 <<<<<<< HEAD
+<<<<<<< HEAD
   get 'crud', to: "crud#index"
 =======
   get 'reports/index', to: "reports#index"
 >>>>>>> 8696622 (Adding initial desing)
+=======
+  get 'reports/index', to: "reports#index"
+>>>>>>> 3af6f1d (Adding initial desing)
   get 'admin', to: "home#index"
   root "employee#employee"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
