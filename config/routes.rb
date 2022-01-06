2fe59be5a720158eddc105d5dc929798773026f8
javs1 <59986245+javs1@users.noreply.github.com>
2022-01-06 17:17:39 -0600

    Adding initial desing
    Added a calendar to represent the employee attendance by day

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+<<<<<<< HEAD
   get 'crud', to: "crud#index"
+=======
+  get 'reports/index', to: "reports#index"
+>>>>>>> 8696622 (Adding initial desing)
   get 'admin', to: "home#index"
   root "employee#employee"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
