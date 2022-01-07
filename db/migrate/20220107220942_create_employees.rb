90ce4ded435b394ba4e693e1c183491bcf97bcd7
Brandon <bvadillo0@ucol.mx>
2022-01-07 16:20:06 -0600

    Created migration for employees
    Created migration for employees table for the database
    the table has
     - string: privateNumber
     - string employeeNumber
     - string email
     - string name
     - string position
     - string store
     - bolean status
     - reference to foreign key for branches

---
+++ db/migrate/20220107220942_create_employees.rb
@@ -0,0 +1,16 @@
+class CreateEmployees < ActiveRecord::Migration[6.1]
+  def change
+    create_table :employees do |t|
+      t.string :privateNumber
+      t.string :employeeNumber
+      t.string :email
+      t.string :name
+      t.string :position
+      t.string :store
+      t.boleean :status
+      t.references :branches, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
