0497928f34d45adfea72492b8fe9589d5c8e8c4a
Brandon <bvadillo0@ucol.mx>
2022-01-10 13:17:40 -0600

    Changed database structure
    Deleted migration CreateEmployeeChecks, also the table EmployeeChecks
    deleted.
    Modified CreateChecks migration and Checks table, added a new column
    that is a foreign key of employees id.
    Modified diagram with the changes.

--- db/migrate/20220109225920_create_checks.rb
+++ db/migrate/20220109225920_create_checks.rb
@@ -4,6 +4,7 @@ class CreateChecks < ActiveRecord::Migration[6.1]
       t.date :date
       t.time :hour
       t.boolean :check
+      t.references :employees, null: false, foreign_key: true
 
       t.timestamps
     end
