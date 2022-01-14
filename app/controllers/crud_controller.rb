61eeb02743ad002f4ae9b4ac6060863e2acf4446
Brandon <bvadillo0@ucol.mx>
2022-01-13 18:47:42 -0600

    Crud backend and fixes done
    Modified routes.rb
     - added post route to logout and destroy session
     - added post route to create and save employee in database+
     - added post route editEmployee to edit employee and save changes
     - added get rout with id to get all employees from a branches in crud
    crud view frontend fixes
     - added modal to add new employees
     - added modal to edit employees
     - added new table row for disable button
     - added new table row for status of the employee
     - deleted search bar
     - the table now updates with the employees of the database
     - branches dropdown now updates with al the branches
    crud controller
     - modified index method to show all employes and branches
     - method logout to destroy session
     - method new to make a new employee
     - method create to save in database a new employee
     - method edit to edit an employee  and save it
     - method disable to disable or enable an employee
    aplication controller changes
     - added a method require_login that in case there no are session does
       redirect to login page
    Created branch model
    Created employee model
    
    Co-authored-by: Joshsam31 <samano82324@gmail.com>
    Co-authored-by: javs1 <javierverduzco13@gmail.com>

--- app/controllers/crud_controller.rb
+++ app/controllers/crud_controller.rb
@@ -1,4 +1,58 @@
-class CrudController < ApplicationController
-  def index
+  class CrudController < ApplicationController
+  
+    def index
+      if params[:id] != nil
+        @employees = Employee.where(branches_id:params[:id])
+      else
+        @employees = Employee.all
+      end
+      @branches = Branch.all
+    end
+    
+    def logout
+      session[:admin_id] = nil
+      redirect_to admin_path
+    end
+  
+    def new
+      @emp = Employee.new
+    end
+  
+    def create
+      @emp = Employee.new(employee_params)
+      if @emp.save
+        redirect_to crud_path
+      else
+        render :new
+      end
+    end
+  
+    def edit
+      @emp = Employee.find(params[:id])
+      if @emp.update(:privateNumber=> params[:privateNumber], :employeeNumber=>params[:employeeNumber],
+        :email=>params[:email],:name=>params[:name],
+        :position=>params[:position],:status=>params[:status],:branches_id=>params[:branches_id])
+        redirect_to crud_path
+      end
+    end
+  
+    def disable
+      @emp = Employee.find(params[:id])
+      if @emp.status == true
+        if @emp.update(:status=>0)
+          redirect_to crud_path
+        end
+      else
+        if @emp.update(:status=>1)
+          redirect_to crud_path
+        end
+      end
+    end
+  
+    private 
+  
+    def employee_params
+      params.permit(:privateNumber, :employeeNumber, :email, :name, :position, :status, :branches_id)
+    end
+  
   end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
   get 'crud', to: "crud#index"
+  get 'crud/:id', to: "crud#index"
   get 'admin', to: "home#index"
+<<<<<<< HEAD
   post 'admin', to: "home#login"
+=======
+  post 'logout', to: "crud#logout"
+  post 'createEmployee', to: "crud#create"
+  post 'editEmployee', to: "crud#edit"
+  post 'disableEmployee', to: "crud#disable"
+>>>>>>> dc20568 (Crud backend and fixes done)
   root "employee#employee"
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
 end
