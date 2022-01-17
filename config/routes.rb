2acfd67126adbb9936e588fa69eed27ec6e7d384
Brandon <bvadillo0@ucol.mx>
2022-01-17 15:19:27 -0600

    Changed name of crud controller
    Changed the name of the crud controller to EmployeeCrudController

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  get 'crud', to: "crud#index"
-  get 'crud/:id', to: "crud#index"
+  get 'employee_status/new'
+  get 'employee_status/create'
+  get 'crud', to: "employee_crud#index"
+  get 'crud/:id', to: "employee_crud#index"
   get 'admin', to: "home#index"
+<<<<<<< HEAD
 <<<<<<< HEAD
   post 'admin', to: "home#login"
 =======
@@ -10,6 +13,12 @@ Rails.application.routes.draw do
   post 'editEmployee', to: "crud#edit"
   post 'disableEmployee', to: "crud#disable"
 >>>>>>> dc20568 (Crud backend and fixes done)
+=======
+  post 'logout', to: "employee_crud#logout"
+  post 'createEmployee', to: "employee_crud#create"
+  post 'editEmployee', to: "employee_crud#edit"
+  post 'disableEmployee', to: "employee_crud#disable"
+>>>>>>> 7f836c5 (Changed name of crud controller)
   root "employee#employee"
 
 end
