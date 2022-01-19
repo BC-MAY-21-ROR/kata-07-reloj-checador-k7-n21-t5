2c91adf0faeb67615f09038dc056f5fbcc47422b
Brandon <bvadillo0@ucol.mx>
2022-01-19 14:20:39 -0600

    Requested changes
    employee_helper
     - changed conditional with a ternary operator in method check_value
    check.rb
     - added blank line
    employee.html.erb
     - deleted html, body and head tags
     - indentation fix

--- app/helpers/employee_helper.rb
+++ app/helpers/employee_helper.rb
@@ -1,17 +1,12 @@
 module EmployeeHelper
 
   def check_value(check)
-    if check
-      1
-    else
-      0
-    end
+    check ? 1 : 0
   end 
 
   def check_save(check)
     if check.save
-      message = params[:check] ? 'Successful check in' : 'Successful check out'
-      message
+      message = params[:check] == 1 ? 'Successful check in' : 'Successful check out'
     else
       'Failed to check'
     end
