f509fab0abb834d8352e2c9b493a7404f9ec40e5
Brandon <bvadillo0@ucol.mx>
2022-01-18 19:00:36 -0600

    Backend and frontend fixes done
    routes.rb
     - added post check route to the employee check controller method
    employee.html.erb
     - modified submit button of check in and check out to sent as parameter
       the name
     - added a notice and alert paragraph to Feedback to employee on
       successful/failed registration
    created Check model
    employee_helper.rb
     - method check_value returns 1 or 0 based on the parameter if true or
       false
     - method check_save save the check in the database and returns the
       feedback message
    employee_controller.rb
     - added check method
     - check method search the employee by the privateNumber from the
       parameter
     - check method save the check and feedback the employee
    
    Co-authored-by: Joshsam31 <samano82324@gmail.com>
    Co-authored-by: javs1 <javierverduzco13@gmail.com>

--- app/controllers/employee_controller.rb
+++ app/controllers/employee_controller.rb
@@ -1,4 +1,26 @@
 class EmployeeController < ApplicationController
   def employee
   end
+
+  def check
+    @employee = Employee.find_by(privateNumber: params[:privateNumber])
+    if !!@employee
+      params[:check] = helpers.check_value(params[:checkin])
+      params[:date] = Time.now.strftime("%d/%m/%Y")
+      params[:hour] = Time.now.strftime("%H:%M")
+      params[:employees_id] = @employee.id
+      @check = Check.new(check_params)
+      redirect_to root_path, notice: helpers.check_save(@check)
+    else
+      redirect_to root_path, alert: 'PrivateNumber does not exist'
+    end
+    
+  end
+  
+  private
+
+  def check_params
+    params.permit(:date, :hour, :check, :employees_id)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get 'crud', to: "crud#index"
   get 'admin', to: "home#index"
   post 'admin', to: "home#login"
+  post 'check', to: "employee#check"
   root "employee#employee"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
