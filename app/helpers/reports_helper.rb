6fd9e84e6e25af80eab3516baa81f25e78588270
Brandon <bvadillo0@ucol.mx>
2022-01-21 17:34:40 -0600

    Fixed reports helper bug and modified view
    reports_helper.rb
     - fixed bug of avg_time method to check the avg time if checks.size is
       bigger than 0
    reports index.html.erb
     - added a href in the navbar for branches path
    
    Co-authored-by: Joshsam31 <samano82324@gmail.com>
    Co-authored-by: javs1 <javierverduzco13@gmail.com>

--- app/helpers/reports_helper.rb
+++ app/helpers/reports_helper.rb
@@ -1,12 +1,14 @@
 module ReportsHelper
 
   def avg_time(checks)
-    avg = checks.size 
-    avg_minutes = checks.map do |p|
-      hour, minute = p.hour.strftime('%H:%M').to_s.split(':')
-      total_minutes = hour.to_i * 60 + minute.to_i
-    end.inject(:+)/avg
-    "#{avg_minutes/60}:#{avg_minutes%60}"
+    if checks.size > 0 
+        avg = checks.size 
+        avg_minutes = checks.map do |p|
+        hour, minute = p.hour.strftime('%H:%M').to_s.split(':')
+        total_minutes = hour.to_i * 60 + minute.to_i
+        end.inject(:+)/avg
+        "#{avg_minutes/60}:#{avg_minutes%60}"
+    end
   end
 
   def absences(checkins,employees)
