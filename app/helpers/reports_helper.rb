846eb4933f5c5837edd0a9637b13c73784da728a
Brandon <bvadillo0@ucol.mx>
2022-01-20 18:56:22 -0600

    Reports model, controller and backend fixes done
    reports.scss
     - deleted calendar class
     - added max height for the table
    aplication_controller.rb
     - added a notice when redirectin to admin login
    employee_crud_controller.rb
     - added before action that requires login
    reports_controller.rb
     - added before action that requires login
     - method index get the absences
     - method index get the atendances
     - method index get checking hour average
     - method index get checkout hour average
    reports_helper.rb
     - avg_time method that returns the average hour
     - absences methot that returns an array of the employee that have
       absences
    aplication.js
     - deleted fullcalendar
    reports.js deleted
    employee_crud index.html.erb
     - modified navbar to redirect to reports_path
    reports index.html.erb
     - deleted full calendar
     - added a table for atendance
     - absence modal shows the employees and their private number
    
    Co-authored-by: javs1 <javierverduzco13@gmail.com>
    Co-authored-by: Joshsam31 <samano82324@gmail.com>

--- app/helpers/reports_helper.rb
+++ app/helpers/reports_helper.rb
@@ -1,2 +1,29 @@
 module ReportsHelper
+
+  def avg_time(checks)
+    avg = checks.size 
+    avg_minutes = checks.map do |p|
+      hour, minute = p.hour.strftime('%H:%M').to_s.split(':')
+      total_minutes = hour.to_i * 60 + minute.to_i
+    end.inject(:+)/avg
+    "#{avg_minutes/60}:#{avg_minutes%60}"
+  end
+
+  def absences(checkins,employees)
+    absences = Array.new
+    employees.each do |e|
+      count = 0
+      checkins.each do |c|
+        if e.id == c.employees_id
+          count +=1
+        end
+      end
+      if count == 0
+        absences.push(e)
+      end
+    end
+    absences
+  end
+  
+
 end
