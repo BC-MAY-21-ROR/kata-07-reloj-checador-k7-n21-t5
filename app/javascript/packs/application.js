eada517828821a0a5e21c2f587067a85cf50ffe3
Brandon <bvadillo0@ucol.mx>
2022-01-21 17:15:52 -0600

    Branches crud frontend, backend and other fixes
    removed vue
    removed jquery from environment.js in webpack config
    routes.rb
     - added branches get for the branches_crud view
     - added post create_branch for the branches#create controller method to
       add a branch
     - added post edit_branch to edit branches with the method edit in
       branch_crud controller
     - added post delete_branch route to delete branch with the metod delete
       in branch_crud controller
    employee_crud index.html.erb
     - added a href for the branches_path in the navbar
    application.js
     - deleted import bootstrap line because it caused an error
     - deleted import aplication.scss because it caused an error
    branch_crud_controller.rb
     - index method that return all branches
     - create method that insert a new branch in database
     - edit method that update a branch in the database
     - delete methot that delete a branch in database
    
    Co-authored-by: javs1 <javierverduzco13@gmail.com>
    Co-authored-by: Joshsam31 <samano82324@gmail.com>

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -12,9 +12,6 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-import "bootstrap"
-import "../stylesheets/application"
-require("packs/employee")
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
